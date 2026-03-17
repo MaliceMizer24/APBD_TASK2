eb42d01d0cfaa221fdf3486233ee69b1234ab99e
MaliceMizer24 <stask6896@gmail.com>
2026-03-17 16:35:04 +0100

    equipment

---
+++ Equipment.cs
@@ -0,0 +1,20 @@
+﻿namespace APBD_TASK2;
+
+abstract class Equipment
+{
+    private static int _nextId = 0;
+    
+    public int Id { get; }
+    
+    public string Name { get; }
+    
+    public string Description { get; }
+
+    public Equipment(string name, string description)
+    {
+        Id = _nextId;
+        _nextId++;
+        Name = name;
+        Description = description;
+    }
+}
