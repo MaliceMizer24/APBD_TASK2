b933a018dc576c05288188a73e29d55869a96954
MaliceMizer24 <stask6896@gmail.com>
2026-03-17 16:38:00 +0100

    laptop

---
+++ Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_TASK2;
+
+public class Laptop
+{
+    public int RamSize { get; set; }
+    
+    public int ScreenSize { get; set; }
+    
+    public Laptop(int ramSize, int screenSize)
+        {
+        RamSize = ramSize;
+        ScreenSize = screenSize;
+        }
+}
