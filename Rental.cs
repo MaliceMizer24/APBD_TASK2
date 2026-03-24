12805b3907eb718fa6a9759f64d766a778ae2e13
malicemizer <stask6896@gmail.com>
2026-03-24 02:32:50 +0100

    Create Rental.cs

---
+++ Rental.cs
@@ -0,0 +1,18 @@
+﻿using APBD_TASK2;
+
+public class Rental
+{
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime RentalDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; set; }
+
+    public Rental(User user, Equipment equipment, DateTime dueDate)
+    {
+        User = user;
+        Equipment = equipment;
+        RentalDate = DateTime.Now;
+        DueDate = dueDate;
+    }
+}
