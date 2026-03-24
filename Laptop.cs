fa099386ae5c89048f810bdd9b076f4ae2ae3288
malicemizer <stask6896@gmail.com>
2026-03-24 02:28:45 +0100

    laptop fix

--- Laptop.cs
+++ Laptop.cs
@@ -1,14 +1,15 @@
 ﻿namespace APBD_TASK2;
 
-public class Laptop
+public class Laptop : Equipment
 {
     public int RamSize { get; set; }
     
     public int ScreenSize { get; set; }
     
-    public Laptop(int ramSize, int screenSize)
-        {
+    public Laptop(string name, string description, int ramSize, int screenSize)
+        : base(name, description)
+    {
         RamSize = ramSize;
         ScreenSize = screenSize;
-        }
+    }
 }
