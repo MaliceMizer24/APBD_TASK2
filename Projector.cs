0cab56bc5d274139f94bdc427b208cb0cc92933f
malicemizer <stask6896@gmail.com>
2026-03-24 02:38:13 +0100

    Projector added

---
+++ Projector.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_TASK2;
+
+public class Projector : Equipment
+{
+    public int Resolution { get; set; }
+    public Projector(string name, string description) : base(name, description)
+    {
+    }
+}
